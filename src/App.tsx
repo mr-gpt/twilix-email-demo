dac9b8d2c9c3086c0e2899b894c72aa48089422a
Jacky Wong <colabdog@gmail.com>
2023-05-27 13:19:22 +1000

    provide package.json

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,23 @@ import {
   Code,
   Grid,
   theme,
+  Textarea,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import Emailform from "./components/emailForm"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        minHeight="100vh"
+        alignItems="center"
+      >
+      <VStack spacing={4}>
+        {/* Include your components inside the VStack with appropriate spacing */}
+          <Emailform/>
+      </VStack>
+    </Box>    
   </ChakraProvider>
 )
