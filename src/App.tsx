8a872e7443b4e7493b469a730b86c8725503dfaf
Jacky Wong <colabdog@gmail.com>
2023-05-27 18:36:00 +1000

    udpate function

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
   VStack,
   theme,
-} from "@chakra-ui/react"
-import Emailform from "./components/emailForm"
+  Button,
+  ButtonGroup,
+  Stack,
+} from "@chakra-ui/react";
+import Emailform from "./components/emailForm";
+import ExtractPlayground from "./components/extractPlayground"
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
+export const App = () => {
+  let [playground, setPlayground] = React.useState("auto-suggest");
+  return (
+    <ChakraProvider theme={theme}>
       <Box
         display="flex"
         flexDirection="column"
-        justifyContent="center"
+        // justifyContent="center"
         minHeight="100vh"
         alignItems="center"
       >
-      <VStack spacing={4}>
-          <Emailform/>
-      </VStack>
-    </Box>    
-  </ChakraProvider>
-)
+        <VStack spacing={4}>
+          <Stack spacing={4} direction="row" align="center">
+            <Button colorScheme="teal" size="xs" onClick={(event: any) => setPlayground("auto-suggest")}
+           >
+              Auto-Suggest Playground
+            </Button>
+            <Button colorScheme="teal" size="xs" onClick={(event: any) => setPlayground("extract")}>
+              Extract Playground
+            </Button>
+          </Stack>
+          {
+            playground == "auto-suggest" &&
+            <Emailform />
+          }
+          {
+            playground == "extract" &&
+            <ExtractPlayground/>
+          }
+        </VStack>
+      </Box>
+    </ChakraProvider>
+  );
+};
--- src/components/emailForm.tsx
+++ src/components/emailForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Box,
   Button,
@@ -9,32 +9,46 @@ import {
   VStack,
   Text,
   Input,
-  Heading
-} from '@chakra-ui/react';
-import axios from 'axios';
+  Heading,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { TwilixClient } from "twilixsdk";
 
 const SimpleForm = () => {
-  const [text, setText] = useState(`James: Hi Bill, on my 21st birthday, I wanted to invite you to my birth party! Can you make it?
+  const [text, setText] =
+    useState(`James: Hi Bill, on my 21st birthday, I wanted to invite you to my birth party! Can you make it?
 ---
 Bill: `);
-  const [apiKey, setApiKey] = useState('');
-  const [responseText, setResponseText] = useState('');
-  const [errorText, setErrorText] = useState('');
+  const [apiKey, setApiKey] = useState("");
+  const [responseText, setResponseText] = useState("");
+  const [errorText, setErrorText] = useState("");
   const [isLoading, setLoading] = useState(false);
-  const collection_name = 'your_collection_name';
+  const [collectionName, setCollectionName] = useState("emailCollection");
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     setLoading(true);
-    setErrorText('');
+    setErrorText("");
     try {
+      // insert the things based on the message
+      const client = new TwilixClient(apiKey);
+      console.log("client started");
+      const bulkInsertResponse = await client.bulkInsert(
+        collectionName,
+        text.split("---").map((m: string) => {
+          return { message: m };
+        })
+      );
+      console.log(bulkInsertResponse);
+
       // run auto suggest on the collection name
       const response = await axios.post(
-        `https://api.twilix.io/collection/${collection_name}/auto_suggest`,
-        { text }, {
-            headers: {
-                Authorization: `Bearer ${apiKey}`
-            }
+        `https://api.twilix.io/collection/${collectionName}/auto_suggest`,
+        { text },
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
         }
       );
       setResponseText(JSON.stringify(response.data, null, 2));
@@ -43,7 +57,8 @@ Bill: `);
       if (axios.isAxiosError(error)) {
         setErrorText(error.message);
       } else {
-        setErrorText("An error occurred.")
+        console.log(error);
+        setErrorText("An error occurred.");
       }
       setLoading(false);
     }
@@ -51,25 +66,25 @@ Bill: `);
 
   return (
     <Container centerContent>
-      <Heading>
-        Email Demo
-      </Heading>
+      <Heading>Email Demo</Heading>
       <form onSubmit={handleSubmit}>
         <VStack width="100%" spacing={4}>
           <FormControl id="text">
-            <FormLabel>API Key (<a href="https://app.twilix.io">Click here to get it</a>) </FormLabel>
+            <FormLabel>
+              API Key (<a href="https://app.twilix.io">Click here to get it</a>){" "}
+            </FormLabel>
             <Input
-                value={apiKey}
-                onChange={(e) => setApiKey(e.target.value)}
-                type="password"
-            >
-            </Input>
+              value={apiKey}
+              onChange={(e) => setApiKey(e.target.value)}
+              type="password"
+            ></Input>
             <FormLabel>Email Input</FormLabel>
             <Textarea
               value={text}
               onChange={(e) => setText(e.target.value)}
               isRequired
               minHeight="100px"
+              maxHeight="600px"
               minWidth="600px"
               maxWidth="800px"
               resize="none"
